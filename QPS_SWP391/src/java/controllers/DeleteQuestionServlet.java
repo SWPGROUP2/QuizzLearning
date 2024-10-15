760264a241df02e72d4c2eca17fe3752ff5127e1
Hieuldhe170444 <hieuldhe170444@fpt.edu.vn>
2024-10-16 02:03:56 +0700

    add delete question function

---
+++ QPS_SWP391/src/java/controllers/DeleteQuestionServlet.java
@@ -0,0 +1,42 @@
+package controllers;
+
+import dal.QuestionDAO;
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author LENOVO
+ */
+
+@WebServlet("/DeleteQuestionServlet")
+public class DeleteQuestionServlet extends HttpServlet {
+ @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String idParam = request.getParameter("id");
+        String subjectIdStr = request.getParameter("subjectId");
+        
+            int questionID = Integer.parseInt(idParam);
+            int subjectId = Integer.parseInt(subjectIdStr);
+            QuestionDAO dao = new QuestionDAO();
+            dao.deleteQuestion(questionID);                    
+           
+            response.sendRedirect("question?id=" + subjectId);          
+    }
+ @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+         String subjectIdStr = request.getParameter("subjectId");
+        request.setAttribute("subjectId", subjectIdStr);
+        doPost(request, response); // Delegate to doPost for deletion logic
+    }
+     @Override
+    public String getServletInfo() {
+        return "Servlet for adding questions";
+    }
+
+}
+
