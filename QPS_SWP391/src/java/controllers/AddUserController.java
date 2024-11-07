b00a9b73c01cce763fec077b36890fc5f58f277b
Hieuldhe170444 <hieuldhe170444@fpt.edu.vn>
2024-11-07 21:47:59 +0700

    final login controller

--- QPS_SWP391/src/java/controllers/AddUserController.java
+++ QPS_SWP391/src/java/controllers/AddUserController.java
@@ -1,122 +1,112 @@
-package controllers;
-
-import dal.UserDAO;
-import java.io.IOException;
-import java.sql.Date;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import models.User;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
-public class AddUserController extends HttpServlet {
-
-    private static final Logger LOGGER = Logger.getLogger(AddUserController.class.getName());
-
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        request.getRequestDispatcher("/signup.jsp").forward(request, response);
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        String mess;
-        try {
-            UserDAO dao = new UserDAO();
-
-            String fullname = request.getParameter("fullname");
-            String email = request.getParameter("email");
-            String role = request.getParameter("role");
-            String password = request.getParameter("password");
-            String phoneNumber = request.getParameter("phonenumber");
-            String place = request.getParameter("place");
-            String dateString = request.getParameter("date");
-            String userCode = request.getParameter("usercode");
-
-            //check blank space
-            if (isBlank(fullname) || isBlank(email) || isBlank(password)
-                    || isBlank(phoneNumber) || isBlank(place) || isBlank(userCode)
-                    || isBlank(dateString)) {
-                mess = "All fields are required and cannot be blank or spaces.";
-                request.setAttribute("mess", mess);
-                request.getRequestDispatcher("/adduser.jsp").forward(request, response);
-                return;
-            }
-
-            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-            java.util.Date utilDate;
-            try {
-                utilDate = sdf.parse(dateString);
-            } catch (ParseException e) {
-                LOGGER.log(Level.SEVERE, "Date parsing failed", e);
-                mess = "Invalid date format.";
-                request.setAttribute("mess", mess);
-                request.getRequestDispatcher("/adduser.jsp").forward(request, response);
-                return;
-            }
-
-            //check user age
-            Date sqlDate = new Date(utilDate.getTime());
-            int age = new java.util.Date().getYear() - utilDate.getYear();
-            if (age < 10) {
-                mess = "You must be at least 10 years old to sign up.";
-                request.setAttribute("mess", mess);
-                request.getRequestDispatcher("/adduser.jsp").forward(request, response);
-                return;
-            }
-            String userName = "User";
-            int roleId;
-            if (role.equals("student")) {
-                roleId = 1;
-            } else {
-                roleId = 3;
-            }
-
-            // Check email exists
-            if (dao.getUserByEmail(email) != null) {
-                mess = "Email is already in use. Please try another email.";
-                request.setAttribute("mess", mess);
-                request.getRequestDispatcher("/adduser.jsp").forward(request, response);
-            }
-            // Check phone number exists
-            if (dao.isPhoneNumberExists(phoneNumber)) {
-                mess = "Phone number is already in use. Please try another number.";
-                request.setAttribute("mess", mess);
-                request.getRequestDispatcher("/adduser.jsp").forward(request, response);
-                return;
-            }
-
-            // Check usercode exists
-            if (dao.isUserCodeExists(userCode)) {
-                mess = "User code is already in use. Please try another code.";
-                request.setAttribute("mess", mess);
-                request.getRequestDispatcher("/adduser.jsp").forward(request, response);
-            } else {
-                User user = new User(roleId, userName, roleId, email, password, role, phoneNumber, place, fullname, sqlDate, place, userCode);
-                dao.addUser(user);
-                mess = "Add User Successful! You can now back to list.";
-                request.setAttribute("mess", mess);
-                request.getRequestDispatcher("/adduser.jsp").forward(request, response);
-            }
-        } catch (Exception ex) {
-            LOGGER.log(Level.SEVERE, "Add failed", ex);
-            request.setAttribute("mess", "An error occurred during sign up. Please try again.");
-            request.getRequestDispatcher("/adduser.jsp").forward(request, response);
-        }
-    }
-
-    private boolean isBlank(String value) {
-        return value == null || value.trim().isEmpty();
-    }
-
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }//
-}
+//package controllers;
+//
+//import dal.UserDAO;
+//import java.io.IOException;
+//import java.sql.Date;
+//import java.text.ParseException;
+//import java.text.SimpleDateFormat;
+//import jakarta.servlet.ServletException;
+//import jakarta.servlet.http.HttpServlet;
+//import jakarta.servlet.http.HttpServletRequest;
+//import jakarta.servlet.http.HttpServletResponse;
+//import models.User;
+//import java.util.logging.Level;
+//import java.util.logging.Logger;
+//
+//public class AddUserController extends HttpServlet {
+//
+//    private static final Logger LOGGER = Logger.getLogger(AddUserController.class.getName());
+//
+//    @Override
+//    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+//            throws ServletException, IOException {
+//        request.getRequestDispatcher("/signup.jsp").forward(request, response);
+//    }
+//
+//    @Override
+//    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+//            throws ServletException, IOException {
+//        String mess;
+//        try {
+//            UserDAO dao = new UserDAO();
+//
+//            String fullname = request.getParameter("fullname");
+//            String email = request.getParameter("email");
+//            String role = request.getParameter("role");
+//            String password = request.getParameter("password");
+//            String phoneNumber = request.getParameter("phonenumber");
+//            String place = request.getParameter("place");
+//            String dateString = request.getParameter("date");
+//            String userCode = request.getParameter("usercode");
+//
+//            //check blank space
+//            if (isBlank(fullname) || isBlank(email) || isBlank(password)
+//                    || isBlank(phoneNumber) || isBlank(place) || isBlank(userCode)
+//                    || isBlank(dateString)) {
+//                mess = "All fields are required and cannot be blank or spaces.";
+//                request.setAttribute("mess", mess);
+//                request.getRequestDispatcher("/adduser.jsp").forward(request, response);
+//                return;
+//            }
+//
+//            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+//            java.util.Date utilDate;
+//            try {
+//                utilDate = sdf.parse(dateString);
+//            } catch (ParseException e) {
+//                LOGGER.log(Level.SEVERE, "Date parsing failed", e);
+//                mess = "Invalid date format.";
+//                request.setAttribute("mess", mess);
+//                request.getRequestDispatcher("/adduser.jsp").forward(request, response);
+//                return;
+//            }
+//
+//            //check user age
+//            Date sqlDate = new Date(utilDate.getTime());
+//            int age = new java.util.Date().getYear() - utilDate.getYear();
+//            if (age < 10) {
+//                mess = "You must be at least 10 years old to sign up.";
+//                request.setAttribute("mess", mess);
+//                request.getRequestDispatcher("/adduser.jsp").forward(request, response);
+//                return;
+//            }
+//            String userName = "User";
+//            int roleId;
+//            if (role.equals("student")) {
+//                roleId = 1;
+//            } else {
+//                roleId = 3;
+//            }
+//
+//            // Check email exists
+//            if (dao.getUserByEmail(email) != null) {
+//                mess = "Email is already in use. Please try another email.";
+//                request.setAttribute("mess", mess);
+//                request.getRequestDispatcher("/adduser.jsp").forward(request, response);
+//            }
+//            // Check phone number exists
+//            if (dao.isPhoneNumberExists(phoneNumber)) {
+//                mess = "Phone number is already in use. Please try another number.";
+//                request.setAttribute("mess", mess);
+//                request.getRequestDispatcher("/adduser.jsp").forward(request, response);
+//                return;
+//            }
+//
+//            // Check usercode exists
+//
+//        } catch (Exception ex) {
+//            LOGGER.log(Level.SEVERE, "Add failed", ex);
+//            request.setAttribute("mess", "An error occurred during sign up. Please try again.");
+//            request.getRequestDispatcher("/adduser.jsp").forward(request, response);
+//        }
+//    }
+//
+//    private boolean isBlank(String value) {
+//        return value == null || value.trim().isEmpty();
+//    }
+//
+//    @Override
+//    public String getServletInfo() {
+//        return "Short description";
+//    }//
+//}
--- QPS_SWP391/src/java/dal/UserDAO.java
+++ QPS_SWP391/src/java/dal/UserDAO.java
@@ -17,6 +17,60 @@ public class UserDAO extends DBContext {
     private PreparedStatement ps;
     private ResultSet rs;
 
+        public User getUserWithStatus(String email, String password) {
+        User user = null;
+        String query = "SELECT u.UserID, "
+                     + "u.UserName, "
+                     + "u.RoleID, "
+                     + "u.Email, "
+                     + "u.Password, "
+                     + "r.Role, "
+                     + "u.StartDate, "
+                     + "u.EndDate, "
+                     + "CASE "
+                     + "WHEN u.StartDate > CURDATE() OR (u.EndDate IS NOT NULL AND u.EndDate < CURDATE()) THEN 'Inactive' "
+                     + "ELSE u.Status "
+                     + "END AS Status "
+                     + "FROM Users u "
+                     + "JOIN Roles r ON u.RoleID = r.RoleID "
+                     + "WHERE u.Email = ? AND u.Password = ?";
+        
+        try (PreparedStatement ps = connection.prepareStatement(query)) {
+            ps.setString(1, email);
+            ps.setString(2, password);
+
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    user = new User();
+                    user.setUserId(rs.getInt("UserID"));
+                    user.setUserName(rs.getString("UserName"));
+                    user.setRoleId(rs.getInt("RoleID"));
+                    user.setEmail(rs.getString("Email"));
+                    user.setPassword(rs.getString("Password"));
+                    user.setRole(rs.getString("Role"));
+                    user.setStartDate(rs.getDate("StartDate"));
+                    user.setEndDate(rs.getDate("EndDate"));
+                    user.setStatus(rs.getString("Status"));
+                }
+            }
+        } catch (SQLException ex) {
+            LOGGER.log(Level.SEVERE, "Error executing query", ex);
+        }
+        return user;
+    }
+
+        public void updateUserStatus(int userId, String status) {
+        String query = "UPDATE Users SET Status = ? WHERE UserID = ?";
+        try (PreparedStatement ps = connection.prepareStatement(query)) {
+            ps.setString(1, status);
+            ps.setInt(2, userId);
+            ps.executeUpdate();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            // Handle exception
+        }
+    }
+
     public boolean isPhoneNumberExists(String phoneNumber) throws SQLException {
         String sql = "SELECT * FROM Users WHERE phoneNumber = ?";
         try ( PreparedStatement statement = connection.prepareStatement(sql)) {
@@ -47,42 +101,6 @@ public class UserDAO extends DBContext {
             throw e;
         }
     }
-
-public List<User> getUser() throws Exception {
-    List<User> users = new ArrayList<>();
-    try {
-        // Updated SQL query to include Status
-        String sqlQuery = "SELECT u.UserID, u.UserName, u.RoleID, u.Email, u.Password, r.Role, u.StartDate, u.EndDate, u.Status "
-                        + "FROM Users u "
-                        + "JOIN Roles r ON u.RoleID = r.RoleID"; // Using JOIN for better readability
-
-        PreparedStatement stm = connection.prepareStatement(sqlQuery);
-        ResultSet rs = stm.executeQuery();
-
-        while (rs.next()) {
-            User u = new User();
-            u.setUserId(rs.getInt("UserID"));
-            u.setUserName(rs.getString("UserName"));
-            u.setRoleId(rs.getInt("RoleID"));
-            u.setEmail(rs.getString("Email"));
-            u.setPassword(rs.getString("Password"));
-            u.setRole(rs.getString("Role"));
-            
-            // Setting the startDate and endDate
-            u.setStartDate(rs.getDate("StartDate"));
-            u.setEndDate(rs.getDate("EndDate"));
-
-            // Setting the status
-            u.setStatus(rs.getString("Status"));
-
-            users.add(u);
-        }
-    } catch (SQLException ex) {
-        Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
-    }
-    return users;
-}
-
 
     public List<User> getTop5NewestUser() throws Exception {
         List<User> top5UserList = new ArrayList<>();
@@ -183,27 +201,6 @@ public User getUserById(int id) {
         statement.setString(2, currentPassword);
         ResultSet rs = statement.executeQuery();
         return rs.next();
-    }
-
-    public User getUser(String email, String password) throws Exception {
-        String sql = "SELECT u.UserID,  u.UserName, u.RoleID , u.Email, u.Password,r.Role\n"
-                + "FROM Users u, Roles r\n"
-                + "WHERE Email =  ? AND Password = ? AND u.RoleID = r.RoleID";
-
-        PreparedStatement stm = connection.prepareStatement(sql);
-        stm.setString(1, email);
-        stm.setString(2, password);
-
-        ResultSet rs = stm.executeQuery();
-        if (rs.next()) {
-            return new User(rs.getInt("UserID"),
-                    rs.getString("userName"),
-                    rs.getInt("RoleID"),
-                    email,
-                    password,
-                    rs.getString("Role"));
-        }
-        return null;
     }
 
     public void updateUserById(String fullName, String userName, String phoneNumber, String dob, int id, String placeWork, String userCode) {
